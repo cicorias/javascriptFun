dd9b6c72c3a75a12dd0f3af60b7d2a7fd49a62d8
Shawn Cicoria <github@cicoria.com>
2015-06-27 17:28:03 -0400

    initial setup

---
+++ src/azMonitor/test/testOne.js
@@ -0,0 +1,11 @@
+
+console.log('test one running');
+
+var assert = require("assert");
+
+describe('Array', function(){
+	it('should return -1', function(){
+		assert.equal(-1, [1,2,3].indexOf(5));
+	})
+	
+})
