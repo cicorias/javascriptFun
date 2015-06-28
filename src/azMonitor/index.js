cab3cc19bd5510e305d330dabdb77b1884daf2cf
Shawn Cicoria <github@cicoria.com>
2015-06-28 18:36:56 -0400

    fix stupid spaces
    code editor is screwy with tab vs. spaces. so for now, i’m using spaces

--- src/azMonitor/index.js
+++ src/azMonitor/index.js
@@ -3,11 +3,11 @@
 console.log('starting...');
 
 var fs = require('fs'),
-	Promise = require("bluebird"),
+    Promise = require("bluebird"),
     xml2js = require('xml2js');
 
 var sm = require('./serviceManager'),
-	cm = require('./certManager');
+    cm = require('./certManager');
 
 var config = require('./privateConfig.json');
 
@@ -16,50 +16,50 @@ var parser = new xml2js.Parser();
 setInterval(getDeployment, config.timeout);
 
 function getServices(){
-	cm.run(config.pemFile)
-		.then(function(certdata){
-			sm.getServices(config.subscriptionId, certdata )
-			.then(function(data2){
-				var body = data2().body;
+    cm.run(config.pemFile)
+        .then(function(certdata){
+            sm.getServices(config.subscriptionId, certdata )
+            .then(function(data2){
+                var body = data2().body;
 
-				parser.parseString(body, function(err, result){
-					if (err) 
-						throw err;
-						
-					console.info('parsing xml done');
-					//console.log(result);	
-				
-				});
-			}).catch(function(e){
-				console.error("error on sm.run: "+ e);
-			});
-		}).catch( function(e) {
-			console.error("error on cm run: " + e);
-		});
+                parser.parseString(body, function(err, result){
+                    if (err) 
+                        throw err;
+                        
+                    console.info('parsing xml done');
+                    //console.log(result);    
+                
+                });
+            }).catch(function(e){
+                console.error("error on sm.run: "+ e);
+            });
+        }).catch( function(e) {
+            console.error("error on cm run: " + e);
+        });
 }
 
 
 function getDeployment(deploymentName){
-	cm.run(config.pemFile)
-		.then(function(certdata){
-			console.log("checking on deployment for : " + config.deploymentName);
-			sm.getDeployment(config.subscriptionId, certdata, config.deploymentName )
-			.then(function(data2){
-				var body = data2().body;
+    cm.run(config.pemFile)
+        .then(function(certdata){
+            console.log("checking on deployment for : " + config.deploymentName);
+            sm.getDeployment(config.subscriptionId, certdata, config.deploymentName )
+            .then(function(data2){
+                var body = data2().body;
 
-				parser.parseString(body, function(err, result){
-					if (err) 
-						throw err;
-						
-					console.info('parsing xml done');
-					//console.log(result);	
-				
-				});
-			}).catch(function(e){
-				console.error("error on sm.run: "+ e);
-			});
-		}).catch( function(e) {
-			console.error("error on cm run: " + e);
-		});
+                parser.parseString(body, function(err, result){
+                    if (err) 
+                        throw err;
+                        
+                    console.info('parsing xml done');
+                    //console.log(result);    
+                
+                });
+            }).catch(function(e){
+                console.error("error on sm.run: "+ e);
+            });
+        }).catch( function(e) {
+            console.error("error on cm run: " + e);
+        });
 }
 
